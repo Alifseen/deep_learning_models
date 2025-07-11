399a26b2810b4a114236d1c872d33c36b4f94f4e
alifseen <alifseen@gmail.com>
2025-07-11 16:36:16 +0500

    Built building blocks for an L layered sigmoid Neural Network and a Classifier using them

---
+++ Basics/.ipynb_checkpoints/nn_building_blocks-checkpoint.py
@@ -0,0 +1,207 @@
+import numpy as np
+import copy
+
+def initialize_parameters(n_x, n_h, n_y):
+    
+    W1 = np.random.randn(n_h, n_x) * 0.01
+    b1 = np.zeros((n_h, 1))
+    W2 = np.random.randn(n_y, n_h) * 0.01
+    b2 = np.zeros((n_y, 1))
+
+    params = {
+        "W1":W1,
+        "b1":b1,
+        "W2":W2,
+        "b2":b2
+    }
+
+    
+    return params
+
+
+
+def initialize_parameters_deep(layer_dims):
+
+    
+    parameters = {}
+    
+    L= len(layer_dims)
+
+    for l in range(1,L):
+        parameters["W" + str(l)] = np.random.randn(layer_dims[l], layer_dims[l-1]) * 0.01
+        parameters["b" + str(l)] = np.zeros((layer_dims[l],1))
+
+        assert(parameters['W' + str(l)].shape == (layer_dims[l], layer_dims[l - 1]))
+        assert(parameters['b' + str(l)].shape == (layer_dims[l], 1))
+    
+    return parameters
+
+def initialize_parameters_deep_xavier(layer_dims):
+
+    np.random.seed(1)
+    parameters = {}
+    L = len(layer_dims)            
+
+    for l in range(1, L):
+        parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l-1]) / np.sqrt(layer_dims[l-1])
+        parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
+        
+        assert(parameters['W' + str(l)].shape == (layer_dims[l], layer_dims[l-1]))
+        assert(parameters['b' + str(l)].shape == (layer_dims[l], 1))
+
+        
+    return parameters
+
+    
+
+def linear_forward(A, W, b):
+
+    Z = np.dot(W, A) +b
+
+    cache = (A, W, b)
+    
+    return Z, cache
+
+
+def sigmoid(Z):
+    A = 1 / (1+np.exp(-Z))
+    cache = Z
+    return A, cache
+
+def relu(Z):
+    A = np.maximum(0, Z)
+    assert(A.shape == Z.shape)
+    cache = Z
+    return A, cache
+
+
+
+def linear_activation_forward(A_prev, W, b, activation):
+
+    if activation == "sigmoid":
+        Z, linear_cache = linear_forward(A_prev, W, b)
+        A, activation_cache = sigmoid(Z)
+    if activation == "relu":
+        Z, linear_cache = linear_forward(A_prev, W, b)
+        A, activation_cache = relu(Z)
+    cache = (linear_cache, activation_cache)
+    
+    return A, cache
+
+
+def L_model_forward(X, parameters):
+
+    A = X
+    L = len(parameters)//2
+    caches = []
+
+    for l in range(1, L):
+        A_prev = A
+        A, cache = linear_activation_forward(A_prev, parameters["W"+str(l)], parameters["b"+str(l)], "relu")
+        caches.append(cache)
+
+    AL, cache = linear_activation_forward(A, parameters["W"+str(L)], parameters["b"+str(L)], "sigmoid")
+    caches.append(cache)
+
+    return AL, caches
+
+
+def compute_cost(AL, Y):
+    m = Y.shape[1]
+    j = -(1/m) * np.sum((Y*np.log(AL)) + ((1-Y)*np.log(1-AL)))
+    j = np.squeeze(j)
+    
+    return j
+
+
+def linear_backward(dZ, cache):
+    A_prev, W, b = cache
+    m = A_prev.shape[1]
+
+    dW = (1/m) * np.dot(dZ, A_prev.T)
+    db = (1/m) * np.sum(dZ, axis=1, keepdims=True)
+    dA_prev = np.dot(W.T, dZ)
+    
+    return dA_prev, dW, db
+
+
+def sigmoid_backwards(dA, cache):
+    Z = cache
+    s = 1 / (1 + np.exp(-Z))
+    dZ = dA * s * (1-s)
+    assert (dZ.shape == Z.shape)
+    return dZ
+
+def relu_backwards(dA, cache):
+    Z = cache
+    dZ = np.array(dA, copy=True)
+    dZ[Z <= 0] = 0
+    assert (dZ.shape == Z.shape)
+    return dZ
+
+def linear_activation_backward(dA, cache, activation):
+    linear_cache, activation_cache = cache
+
+    if activation == "relu":
+        dZ = relu_backwards(dA, activation_cache)
+        dA_prev, dW, db = linear_backward(dZ, linear_cache)
+    if activation == "sigmoid":
+        dZ = sigmoid_backwards(dA, activation_cache)
+        dA_prev, dW, db = linear_backward(dZ, linear_cache)
+
+    return dA_prev, dW, db
+    
+def L_model_backward(AL, Y, caches):
+    grads = {}
+    L = len(caches)
+    m = AL.shape[1]
+    Y = Y.reshape(AL.shape)
+
+    dAL = -(np.divide(Y, AL) - np.divide(1 - Y, 1 - AL))
+
+    current_cache = caches[-1]
+    dA_prev_temp, dW_temp, db_temp = linear_activation_backward(dAL, current_cache, "sigmoid")
+    grads["dA" + str(L-1)] = dA_prev_temp
+    grads["dW" + str(L)] = dW_temp
+    grads["db" + str(L)] = db_temp
+    
+    
+    for l in range(L-2, -1, -1):
+        dA_prev = dA_prev_temp
+        current_cache = caches[l]
+        dA_prev_temp, dW_temp, db_temp = linear_activation_backward(dA_prev, current_cache, "relu")
+        grads["dA" + str(l)] = dA_prev_temp
+        grads["dW" + str(l+1)] = dW_temp
+        grads["db" + str(l+1)] = db_temp        
+        
+    
+    return grads
+
+
+def update_parameters(params, grads, learning_rate):
+    parameters = copy.deepcopy(params)
+    L = len(params)//2
+    
+    for l in range(1, L+1):
+        parameters["W"+str(l)] = parameters["W"+str(l)] - (learning_rate * grads["dW"+str(l)])
+        parameters["b"+str(l)] = parameters["b"+str(l)] - (learning_rate * grads["db"+str(l)])
+
+    return parameters
+    
+
+def predict(X, y, parameters):
+    m = X.shape[1]
+    n = len(parameters) // 2 
+    p = np.zeros((1,m))
+    
+    probas, caches = L_model_forward(X, parameters)
+
+    for i in range(0, probas.shape[1]):
+        if probas[0,i] > 0.5:
+            p[0,i] = 1
+        else:
+            p[0,i] = 0
+
+    print("Accuracy: "  + str(np.sum((p == y)/m)))
+        
+    return p
